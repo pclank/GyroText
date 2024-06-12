0d169693ecfe12a67439edadb59523643a866fea
pclank <31415965+pclank@users.noreply.github.com>
2024-06-12 03:45:25 +0200

    fixed crash, and added temp ending.
    Signed-off-by: pclank <31415965+pclank@users.noreply.github.com>

--- app/src/main/java/com/example/gyrotext/MainActivity.kt
+++ app/src/main/java/com/example/gyrotext/MainActivity.kt
@@ -694,6 +694,16 @@ class MainActivity : ComponentActivity() {
     // On copy action of correct text
     private fun switchExperiment()
     {
+        // TODO: End experiment!
+        if (experimentList.size == 1)
+        {
+            Toast.makeText(
+                this, R.string.exp_end_temp,
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
+
         // Update list
         experimentList.removeAt(current_exp_id)
 
